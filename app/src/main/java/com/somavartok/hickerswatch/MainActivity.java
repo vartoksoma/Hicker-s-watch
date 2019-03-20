2fbdbdc8999060abb8388ba6eae7c6b09b658096
Vartok Soma <vartok.soma@gmail.com>
2019-03-20 10:56:41 +0100

    setting up design

--- app/src/main/java/com/somavartok/hickerswatch/MainActivity.java
+++ app/src/main/java/com/somavartok/hickerswatch/MainActivity.java
@@ -1,13 +1,44 @@
 package com.somavartok.hickerswatch;
 
+import android.Manifest;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
     }
 }
